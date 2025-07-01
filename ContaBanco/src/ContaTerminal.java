b56a64cc9907e9f0eeb8e839d60ab539a900a472
Helio Franca <heliordf@hotmail.com>
2025-07-01 16:37:24 -0300

    Feature: ContaTerminal

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,6 +1,29 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // consome o '\n' que fica após o nextInt()
+
+        System.out.print("Digite a agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o saldo: ");
+        BigDecimal saldo = scanner.nextBigDecimal();
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d, e seu saldo  R$ %.2f%n já está disponível para saque",
+                nomeCliente, agencia, numero, saldo);
+
+        scanner.close();
+
     }
-    
+
 }
